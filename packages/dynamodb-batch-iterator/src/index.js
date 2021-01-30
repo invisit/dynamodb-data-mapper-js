19c013459c039396997e086d130df55aefc7f1e0
Jonathan Glanz <jonathan.glanz@gmail.com>
2021-01-30 16:52:49 -0500

    wip

--- packages/dynamodb-batch-iterator/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./BatchGet"), exports);
-tslib_1.__exportStar(require("./BatchGetOptions"), exports);
-tslib_1.__exportStar(require("./BatchWrite"), exports);
-tslib_1.__exportStar(require("./types"), exports);
-//# sourceMappingURL=index.js.map
